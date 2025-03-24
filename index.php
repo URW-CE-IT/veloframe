c1d938f4ea846256fc346035876569bccb1c6d46
tuxnull <patrick@garske.link>
2025-03-24 17:57:56 +0100

    Add routing manager for phps cli-server to index.php

--- index.php
+++ index.php
@@ -21,6 +21,16 @@ define("DEBUG", 1);                         # 0: No Debug; 1: WARNings only; 2: 
 define("ALLOW_INLINE_COMPONENTS", TRUE);    # Inline Component Processing could impact performance. To improve performance, you can disable it if its not needed.
 
 $path = "index";
+if(php_sapi_name() == 'cli-server') {
+    $path = substr($_SERVER['REQUEST_URI'], 1);
+    if(is_file(__DIR__."/".$path)) {
+        if(preg_match('/.(js|css|png|jpe?g|gif|svg|ico|webp|woff2?|ttf|otf|eot|mp4|mp3|wav|avi|mov|pdf|zip|rar|md)$/', $path)) {
+            echo file_get_contents(__DIR__."/".$path);
+        }
+    }
+    if(empty($path)) $path = "index";
+}
+
 if(isset($_GET["rpath"])){
     $path = $_GET["rpath"];
 }
